4168def9cf7cbe0e3c33e1cedc1daccd3ef7a321
Robert Lee <bobbydwise1@hotmail.com>
2019-08-17 23:04:52 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Daily Coding Problem 2019-08-17
+
+####  _Good morning! Here's your coding interview problem for today._
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Dropbox.
+
+Given the root to a binary search tree, find the second largest node in the tree.
---
+++ scripts.js
@@ -0,0 +1,17 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Dropbox.
+
+Given the root to a binary search tree, find the second largest node in the tree.
+*/
+
+
+
+$(document).ready(function() {
+  $('#form1').submit(function(event) {
+  event.preventDefault();
+  let input1 = $('#input1').val();
+  $('#output-section-1').text(input1);
+  })
+});
